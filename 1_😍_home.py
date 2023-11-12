bca92fb6771ddd518fcbcf16c8ce0e940ec25250
Harsh  Vaidya <harshvaidya432004@gmail.com>
2023-11-12 10:38:19 +0530

    first commit

---
+++ 1_😍_home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Multiple page",
+    page_icon="👋",
+    layout='wide',
+)
+st.title = ("Multiple pages")
+st.sidebar.success("Select a page above")
